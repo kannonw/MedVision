1a510b26e61053a69aae23b59fc385fe4ee1031c
kannon.marih@protonmail.com <kannon.marih@gmail.com>
2023-09-29 22:19:32 -0300

    Using FileSystem session type

--- app/__init__.py
+++ app/__init__.py
@@ -2,23 +2,23 @@ from flask import Flask
 from flask_session import Session
 
 from config import Config
-from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
 
 
 def create_app():
     app = Flask(__name__)
 
-    db = SQLAlchemy()
+    # db = SQLAlchemy()
 
     app.config.from_object(Config)
-    app.config['SESSION_SQLALCHEMY'] = db
+    # app.config['SESSION_SQLALCHEMY'] = db
 
     Session(app)
-    db.init_app(app)
+    # db.init_app(app)
 
     with app.app_context():
         from app.views import main
         app.register_blueprint(main)
-        db.create_all()
+        # db.create_all()
 
     return app
--- config.py
+++ config.py
@@ -1,11 +1,13 @@
+from datetime import timedelta
+
 class Config:
     MAX_CONTENT_LENGHT = 1024 * 1024 * 8 # 8 MB
     SECRET_KEY = 'secret-key'
 
     # SQLAlchemy
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
 
     # Session
     # SESSION_PERMANENT = True
-    # PERMANENT_SESSION_LIFETIME = timedelta(hours=1)
-    SESSION_TYPE = 'sqlalchemy'
+    PERMANENT_SESSION_LIFETIME = timedelta(seconds=300)
+    SESSION_TYPE = 'filesystem'
